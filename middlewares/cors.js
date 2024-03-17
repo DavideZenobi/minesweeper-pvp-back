1791511a819f02efe3d6caf56f0f849bd6f5815b
davide <davidezenobigarcia@gmail.com>
2024-03-17 15:33:56 +0100

    Cors update and procfile

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 export const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    'https://www.minesweeperpvp.online:443',
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
